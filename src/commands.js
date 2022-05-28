8aa4a4a8fef8c13fe763755ba1699174b6cb7c7c
Pegos <steliospegos08@gmail.com>
2022-05-29 02:37:15 +0300

    final version

---
+++ src/commands.js
@@ -0,0 +1,66 @@
+var telegram = "https://t.me/SteliosPegos";
+var github = "https://github.com/PegosStelios";
+var email = 'stelios@pegos.net';
+
+whois = [
+  "<br>",
+  "Hey, Pegos (aka Stelios Pegos)! 🎩",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'telegram      <a href="' + telegram + '" target="_blank">telegram/SteliosPegos' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/PegosStelios' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Pegos?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           duh',
+  '<span class="command">email</span>          Always open for new stuff!',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the cool art',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Pegos Stelios | An aspiring IT Applications Technician.</span><br>',
+  "        .e$$$$$$$$$o.  .z+^^-eu.      .xu..",
+  "     d$$$$$$$$$$$$$$$$$o.    $$$$$u#         ^%s",
+  "   u$$$$$$$$$$$$$$$$$$$$$$.  $$$$^              ^s           ___                   ___          ___             ",
+  "  u$$$$$$$$$$$$$$$$$$$$$$$$u $$$                  ^.        /\\  \\        ___      /\\__\\        /\\__\\      ",
+  "  $$$$$$$$$$$$$$$$$$$$$$$$$$c$$                    'c      /::\\  \\      /\\  \\    /::|  |      /:/  /        ",
+  "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$               :       k     /:/\\:\\  \\     \\:\\  \\  /:|:|  |     /:/__/       ",
+  " $$$$$$$$$$$$$$$$$$$$$$$$$$$$$              :z!      '>   /::\\~\\:\\  \\    /::\\__\\/:/|:|  |__  /::\\__\\____",
+  "$$$$$$$$$$$$$$$$$$$$$$$$$$$$EB             R         5  /:/\\:\\ \\:\\__\\__/:/\\/__/:/ |:| /\\__\\/:/\\:::::\\__\\",
+  "$$$$$$$$^RR$$$$$$$$$$$$$$$$$E'            :E         9  \\/__\\:\\/:/  /\\/:/  /  \\/__|:|/:/  /\\/_|:|~~|~",
+  " $$$$$$P   $$$$$$$$$$$$$$$$$$E k           ' `%       F       \\::/  /\\::/__/       |:/:/  /    |:|  |",
+  "$$$$$&    9$$$$$$$$$$$$$$$$$E ?           '   *     z         \\/__/  \\:\\__\\       |::/  /     |:|  |",
+  " $$$$      9$$$$$$$$$$$$$$$$$`  $           >   k   X                  \\/__/       /:/  /      |:|  |",
+  " d**Nx     $$$$$$$$$$$$$$$$$$   ''          5   ?  f                               \\/__/        \\|__|",
+  "$$  ?Lk   :$$$$$$$$$$$$$$$$$$               `  u#$$N",
+  "RRF $5^   8$$$$$$$$$$$$$$$$$$                E?&9$$FE",
+  " ^**#`    $$$$$$$$$$$$$$$$$$$                ? ^$$$@`",
+  "         J$*$$$$$$$$$$$$$$$$$                 L",
+  "        x$$$$c#$$$$**#^^^^^^^$$$$$$$$$$$$eed^R",
+  '<span class="color2">Welcome to my website.</span>',
+  "<span class=\"color2\">For a list of availabl1e commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
